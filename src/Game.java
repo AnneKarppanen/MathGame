da18dade297cc03d59179c9bdb07b503aab98fea
Anne Karppanen <aekarppanen@gmail.com>
2022-05-01 18:38:16 +0300

    Updated GameData

--- src/Game.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class Game {
-    private User user;
-    private int questionNumber;
-    private int wrongAnswers;
-    private ArrayList<String> questionsToAskAgain;
-
-}

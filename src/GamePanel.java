0d27e9762ec87872f96c41a3fa1eeb27f53b689d
Maikki Tarkkonen <mtarkkonen@hotmail.com>
2022-05-03 18:56:32 +0300

    Maikkis latest

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -6,16 +6,17 @@ public class GamePanel extends JPanel {
 
     private JButton checkButton = null;
     private ImageIcon coinPicture = null;
+    private ImageIcon timePicture = null;
     private JLabel coinImageLabel = null;
+    private JLabel timeImageLabel = null;
     private JLabel pointsLabel = null;
-    //private GameController gameController;
+    private JLabel timeLabel = null;
+    private GameController gameController;
 
 
     public GamePanel(){
         //this.gameController = gameController;
-        //this.gameController = gameController.getInstance();
-
-     //   this.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        this.gameController = gameController.getInstance();
         this.setBackground(new Color(237, 243, 249));
         this.setLayout(new BorderLayout());
         JPanel middlePane = new JPanel();
@@ -24,37 +25,59 @@ public class GamePanel extends JPanel {
         middlePane.setBackground(new Color(237, 243, 249));
         middlePane.setLayout(new BorderLayout());
         this.add(middlePane, BorderLayout.CENTER);
-      //  southPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         southPane.setBackground(new Color(50, 34, 151));
-        southPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-        southPane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+     southPane.setLayout(new BorderLayout());
+     //   southPane2.setLayout(new FlowLayout(FlowLayout.LEFT));
+     //   southPane2.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
 
         coinPicture = new ImageIcon("src/images/coin_small.png");
+        timePicture = new ImageIcon("src/images/time_small.png");
         JPanel pointsPanel = new JPanel();
+        JPanel timePanel = new JPanel();
         pointsPanel.setBackground(new Color(50, 34, 151));
+        timePanel.setBackground(new Color(50, 34, 151));
+        timePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+      
+        
+      
+  
+        JLabel timeTitle = new JLabel("00:12", JLabel.CENTER);
+        JLabel pointsTitle = new JLabel("5000", JLabel.CENTER);
+        timeTitle.setForeground(new Color(255, 255, 255));
+        timeTitle.setFont(new Font("Arial", Font.BOLD, 15));
+        pointsTitle.setForeground(new Color(255, 255, 255));
+        pointsTitle.setFont(new Font("Arial", Font.BOLD, 15));
+
+        
+      //  JPanel emptyPanel = new JPanel();
+      //  emptyPanel.setPreferredSize(new Dimension(1010, 10));
+      //  emptyPanel.setBackground(new Color(50, 34, 151));
+      //  emptyPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
         //Tähän pointLabeliin pitää nyt sitten saada tuotua peliscore...
         //Gamedata.getPOints?
-        pointsLabel = new JLabel("test");
-        pointsLabel.setBackground(new Color(255, 255, 255));
-        pointsLabel.setPreferredSize(new Dimension(50, 10));
+       // pointsLabel = new JLabel("test");
+      //  pointsLabel.setBackground(new Color(255, 255, 255));
+      //  pointsLabel.setPreferredSize(new Dimension(50, 10));
         
         coinImageLabel = new JLabel();
+        timeImageLabel = new JLabel();
         coinImageLabel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        timePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        timePanel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
         
         coinImageLabel.setIcon(coinPicture);
+        timeImageLabel.setIcon(timePicture);
         
         pointsPanel.add(coinImageLabel);
-        pointsPanel.add(pointsLabel);
+        pointsPanel.add(pointsTitle);
+        timePanel.add(timeImageLabel);
+        timePanel.add(timeTitle);
+     //   southPane2.setOpaque(false);
+    //    southPane2.add(timePanel);
 
         
         this.add(southPane, BorderLayout.SOUTH);
-
-        
-       
-   //     GridBagConstraints cConstraints = new GridBagConstraints();
-    //    cConstraints.insets = new Insets(20, 20, 20, 20);
-    //    cConstraints.gridx = 0;
-    //    cConstraints.gridy = 0;
+     //   this.add(southPane2, BorderLayout.SOUTH);
 
     
  
@@ -62,12 +85,6 @@ public class GamePanel extends JPanel {
         checkButton = new JButton("TARKISTA");
         checkButton.setBackground(new Color(255, 164, 58));
         checkButton.setFont((new Font("Arial", Font.BOLD, 20)));
-     //   checkButton.setPreferredSize(new Dimension(20, 10));
-     //   cConstraints.gridx = 0;
-     //   cConstraints.gridy = 0;
-     //   cConstraints.ipadx = 60; // internal padding x
-     //   cConstraints.ipady = 25; // internal padding y
-     //   checkButton.add(cConstraints);
         checkButton.setHorizontalAlignment(SwingConstants.CENTER);
         JPanel southArea = new JPanel();
         southArea.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -76,18 +93,19 @@ southArea.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
         southArea.add(checkButton);
      //   southArea.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
      
+JPanel quizPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+JLabel question = new JLabel("5 + 5 = ");
+quizPanel.add(question);
 
 
         middlePane.add(southArea,BorderLayout.SOUTH);
+        middlePane.add(quizPanel, BorderLayout.CENTER);
         
         
-        southPane.add(pointsPanel);
-       // southPane.add(pointsLabel);
+        southPane.add(pointsPanel, BorderLayout.EAST);
+        southPane.add(timePanel, BorderLayout.WEST);
+
         
-       
-    //   this.add(middlePane, BorderLayout.CENTER);
-    //   middlePane.add(southPane, BorderLayout.SOUTH);
-    //   southPanel.add(checkButton);
 
         setUpButtonListeners();
 
